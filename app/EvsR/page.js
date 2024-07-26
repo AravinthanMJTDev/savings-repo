9b27bedefbab8ab1658784e4f53f0116cff10e26
AravinthanMJTDev <mjtechgit@gmail.com>
2024-07-26 16:01:11 +0530

    change

--- app/EvsR/page.js
+++ app/EvsR/page.js
@@ -1,16 +1,50 @@
+"use client"
+import { useState } from 'react';
 import React from 'react'
 
 export default function EvsRsaving() {
-    const EstimatedSavings = 30.00;
-    const RealizedSavings = 25.5;
+    const [EstimatedSavings, setEstimated] = useState(30.00);
+    const [RealizedSavings, setRealized] = useState(25.5);
+
+    const changeValue = (e) => {
+        e.preventDefault();
+        console.log(e);
+        switch (e.target.value) {
+            case '1D': {
+                setEstimated(25.67);
+                setRealized(15.87);
+            }
+                break;
+            case '1W': {
+                setEstimated(56.67);
+                setRealized(25.87);
+            }
+                break;
+            case '1M': {
+                setEstimated(91.67);
+                setRealized(75.87);
+            }
+                break;
+            case '3M': {
+                setEstimated(90.7);
+                setRealized(85.87);
+            }
+                break;
+        }
+    }
     return (
         <>
             <div className="container flex flex-col justify-evenly bg-gray-200 pb-5 pl-5 pr-5 border border-t-white border-t-4 rounded-lg border-b-white border-b-4 border-l-white border-l-2 border-r-white border-r-2">
                 <div className='flex flex-row justify-between p-2'>
                     <h5 className="ml-5">Savings: Estimated vs Realized</h5>
 
-                    <div>
-                        <button></button>
+                    <div className="flex flex-row gap-4">
+                        <div className="flex flex-row gap-1">
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="1D" onClick={changeValue}>1D</button>
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="1W" onClick={changeValue}>1W</button>
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="1M" onClick={changeValue}>1M</button>
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="3M" onClick={changeValue}>3M</button>
+                        </div>
                         <select>
                             <option></option>
                         </select>
--- app/totalsavings/page.js
+++ app/totalsavings/page.js
@@ -1,17 +1,60 @@
+"use client"
+import { useState } from 'react';
 import React from 'react'
 
 export default function TotalSavings() {
-    const EnergySavings = 4030;
-    const FinancialSavings = "96,782";
-    const co2Savings = "2,68,342";
+    const [EnergySavings, setEnergy] = useState(4030);
+    const [FinancialSavings, setFinancial] = useState("96,782");
+    const [co2Savings, setCO2] = useState("2,68,342");
+
+    const changeValue = (e) => {
+        e.preventDefault();
+        console.log(e);
+        switch (e.target.value) {
+            case '1D': {
+                setEnergy(2567);
+                setFinancial("15,879");
+                setCO2("2,34,154")
+            }
+                break;
+            case '1W': {
+                setEnergy(5667);
+                setFinancial("25,887");
+                setCO2("1,23,456")
+            }
+                break;
+            case '1M': {
+                setEnergy(9167);
+                setFinancial("75,087");
+                setCO2("3,12,321")
+            }
+                break;
+            case '3M': {
+                setEnergy(9097);
+                setFinancial("85,987");
+                setCO2("4,23,134")
+            }
+                break;
+        }
+    }
+
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <div className="container flex flex-col justify-center bg-gray-200 pb-5 pl-5 pr-5 border border-t-white border-t-4 rounded-lg border-b-white border-b-4 border-l-white border-l-2 border-r-white border-r-2">
                 <div className='flex flex-row justify-between p-2'>
                     <h5 className="ml-5">Total Savings</h5>
 
-                    <div>
-                        <button></button>
+                    <div className="flex flex-row gap-4">
+                        <div className="flex flex-row gap-0 bg-gray-300 rounded-lg">
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="1D" onClick={changeValue}>1D</button>
+                            <div className="w-px bg-gray-400"></div>
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="1W" onClick={changeValue}>1W</button>
+                            <div className="w-px bg-gray-400"></div>
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="1M" onClick={changeValue}>1M</button>
+                            <div className="w-px bg-gray-400"></div>
+                            <button className="w-8 h-8 bg-gray-300 rounded-lg" value="3M" onClick={changeValue}>3M</button>
+
+                        </div>
                         <select>
                             <option></option>
                         </select>
